4466d09ff7f33eb8b769338aedf7fde68adeccf0
zkxshg <zkxshg@126.com>
2021-06-13 16:12:13 +0800

    Update Math_AngleOfClock.cpp

--- Math_AngleOfClock.cpp
+++ Math_AngleOfClock.cpp
@@ -1,3 +1,5 @@
+// Return the angle difference in temp time
+
 double angleClock(int hour, int minutes) {
     if (hour == 12) hour = 0;
     double result = abs((30 - 30 * minutes / 60.0) + (minutes - (hour + 1) * 5) * 6);
