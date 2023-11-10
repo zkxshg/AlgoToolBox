7bc9c6f8dabeb9a18ca0d09e203b462795a40e84
zkxshg <zkxshg@126.com>
2023-11-11 04:02:25 +0800

    Update Array_2DRangeSum.cpp

--- Array_2DRangeSum.cpp
+++ Array_2DRangeSum.cpp
@@ -8,9 +8,35 @@
 // Prepare the prefix sum matrix
 vector<vector<int> > P;
 
+vector<vector<int>> getPsum(vector<vector<int>>& matrix) {
+    int m = matrix.size(), n = matrix[0].size();
+
+    // pre sum
+    vector<vector<int> > psum(m, vector<int>(n));
+
+    psum[0][0] = matrix[0][0]; // (0,0)
+
+    for (int i = 1; i < m; i++) psum[i][0] = psum[i - 1][0] + matrix[i][0]; // (i,0)
+    for (int i = 1; i < n; i++) psum[0][i] = psum[0][i - 1] + matrix[0][i]; // (0,i)
+
+    // (i,j)
+    for (int i = 1; i < m; i++)
+        for (int j = 1; j < n; j++)
+            psum[i][j] = psum[i - 1][j] + psum[i][j - 1] + matrix[i][j] - psum[i - 1][j - 1];
+
+    return psum;
+}
+
+int subSum(vector<vector<int>>& ps, int x1, int y1, int x2, int y2) {
+    int res = ps[x2][y2];
+    if (x1 > 0) res -= ps[x1 - 1][y2];
+    if (y1 > 0) res -= ps[x2][y1 - 1];
+    if (x1 && y1) res += ps[x1 - 1][y1 - 1];
+    return  res;
+}
 
 // getPsum function constructs a prefix sum matrix from the input matrix
-vector<vector<int>> getPsum(vector<vector<int>>& matrix) {
+vector<vector<int>> getPsum_Comm(vector<vector<int>>& matrix) {
     int m = matrix.size(), n = matrix[0].size();
 
     // Initialize the prefix sum matrix with the same dimensions as the input matrix
@@ -36,7 +62,7 @@ vector<vector<int>> getPsum(vector<vector<int>>& matrix) {
 }
 
 // subSum function returns the sum of the submatrix bounded by (x1, y1) and (x2, y2) in the original matrix
-int subSum(vector<vector<int>>& ps, int x1, int y1, int x2, int y2) {
+int subSum_Comm(vector<vector<int>>& ps, int x1, int y1, int x2, int y2) {
     // Start with the pre-calculated sum up to (x2, y2)
     int res = ps[x2][y2];
 
@@ -49,31 +75,4 @@ int subSum(vector<vector<int>>& ps, int x1, int y1, int x2, int y2) {
 
     // Return the sum of the submatrix
     return res;
-}
-
-vector<vector<int>> getPsum_raw(vector<vector<int>>& matrix) {
-    int m = matrix.size(), n = matrix[0].size();
-
-    // pre sum
-    vector<vector<int> > psum(m, vector<int>(n));
-
-    psum[0][0] = matrix[0][0]; // (0,0)
-
-    for (int i = 1; i < m; i++) psum[i][0] = psum[i - 1][0] + matrix[i][0]; // (i,0)
-    for (int i = 1; i < n; i++) psum[0][i] = psum[0][i - 1] + matrix[0][i]; // (0,i)
-
-    // (i,j)
-    for (int i = 1; i < m; i++)
-        for (int j = 1; j < n; j++)
-            psum[i][j] = psum[i - 1][j] + psum[i][j - 1] + matrix[i][j] - psum[i - 1][j - 1];
-
-    return psum;
-}
-
-int subSum_raw(vector<vector<int>>& ps, int x1, int y1, int x2, int y2) {
-    int res = ps[x2][y2];
-    if (x1 > 0) res -= ps[x1 - 1][y2];
-    if (y1 > 0) res -= ps[x2][y1 - 1];
-    if (x1 && y1) res += ps[x1 - 1][y1 - 1];
-    return  res;
 }
