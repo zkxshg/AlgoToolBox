2d2f90427389824285fc29d0bda67c83854d1bc3
zkxshg <zkxshg@126.com>
2023-11-11 07:36:44 +0800

    Update Bit_BitMask.cpp

--- Bit_BitMask.cpp
+++ Bit_BitMask.cpp
@@ -1,5 +1,13 @@
 // Using bit mask to generate all subsequences of string
 
+// Goal of Algorithm:
+The algorithm's purpose is to generate all subsequences of a given string characters of a specific length combinationLength.
+
+// Input: a string characters and an integer combinationLength as inputs.
+// Output: a vector of strings, which are all the subsequences of characters of length combinationLength.
+    
+// Time complexity: O(2^n * n). Space complexity: O(2^n * n).
+    
 vector<string> generAll(string characters, int combinationLength) {
     int len = characters.size();
     int maskLen = 1 << len;
