f29e67aa4b47d64f7efceae0c3d24599d1e5a746
zkxshg <zkxshg@126.com>
2023-11-11 04:51:18 +0800

    Update Array_ReshapeMatrix .cpp

--- Array_ReshapeMatrix .cpp
+++ Array_ReshapeMatrix .cpp
@@ -1,5 +1,14 @@
 // Reshape an m x n matrix into a new one with a different size r x c keeping its original data.
 
+// The goal of this algorithm is to reshape an m x n matrix into a different r x c size matrix,
+// while preserving the original data.
+
+// Input: The algorithm takes as input a 2D matrix mat and two integers r and c,
+// which represent the number of rows and columns of the new reshaped matrix respectively.
+
+// Output: The output of the algorithm is a reshaped 2D matrix of size r x c. 
+// If the reshaping isn't possible, the original matrix is returned.
+
 vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
     int m = mat.size(), n = mat[0].size(); if (m * n != r * c) return mat;
     
