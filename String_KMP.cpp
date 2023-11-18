5f933bdde492075f18b9a1c037732d5c617e4dd7
zkxshg <zkxshg@126.com>
2023-11-19 01:13:59 +0800

    Update String_KMP.cpp

--- String_KMP.cpp
+++ String_KMP.cpp
@@ -5,26 +5,33 @@ class Solution {
 public:
 
     int KMP(string haystack, string needle) {
-        int lenh = haystack.size();
-        int lens = needle.size();
-        
+        int lenh = haystack.size(); // Length of the haystack
+        int lens = needle.size(); // Length of the needle
+
+         // Edge cases checking
         if (!lens) return 0;
         else if (lenh < lens) return -1;
-        
+
+        // If haystack and needle are of same length, check if they are equal
         if (lenh == lens){
             for (int j = 0; j < lens; j++)
                 if (haystack[j] != needle[j]) 
                     return -1;
             return 0;
         }
-        
+
+        // Construct prefix table for needle
         vector<int> Ti(lens+1, 0); Ti[0] = -1;
-        
+
+        // Constructing prefix table
         int i = 1;
         while (i < lens) {
+            // If the current character matches the first character
             if (needle[i] == needle[0]){
                 int j = 0, count = 1;
                 if (Ti[i+1] < 1) Ti[i+1] = 1;
+                
+                // Check for larger prefix matches
                 while (i + count < lens - 1 && needle[j + count] == needle[i + count]) {
                     if (count + 1 > Ti[i + count + 1]) Ti[i + count + 1] = count + 1;
                     count++;
@@ -32,18 +39,23 @@ public:
             }
             i++;
         }
-        
+
+        // Main KMP algorithm part for pattern matching
         int result = -1;
         i = 0;
         while (i < lenh){
+            // If the first characters match
             if (haystack[i] == needle[0]){
                 if (lens == 1) return i;
+                
                 int count = 1;
+                // While characters match
                 while (haystack[i + count] == needle[count]) {
                     count++;
                     if (count == lens) return i;
                     else if (i + count >= lenh) return -1;
                 }
+                // Jump forward in haystack according to prefix table
                 i = i + count - Ti[count];
             }
             else i++;
